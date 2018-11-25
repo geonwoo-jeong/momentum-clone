9d9ca78a45d568c90a8fbd80c273b90bdfa8c6a7
Geonwoo-Jeong <geonwoo.jeong@gmail.com>
2018-11-25 17:06:11 +0900

    Saving the User Name part Two

--- greeting.js
+++ greeting.js
@@ -1,5 +1,18 @@
 var form = document.querySelector(".js-form"), input = form.querySelector("input"), greeting = document.querySelector(".js-greetings");
 var USER_LS = "currentUser", SHOWING_CN = "showing";
+function setName(name) {
+    localStorage.setItem(USER_LS, name);
+}
+function handleSubmit(event) {
+    event.preventDefault();
+    var currentName = input.value;
+    paintGreeting(currentName);
+    setName(currentName);
+}
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
@@ -8,7 +21,7 @@ function paintGreeting(text) {
 function loadName() {
     var currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
-        // not
+        askForName();
     }
     else {
         paintGreeting(currentUser);
--- greeting.ts
+++ greeting.ts
@@ -5,6 +5,22 @@ const form = <HTMLFormElement>document.querySelector(".js-form"),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+function setName(name) {
+  localStorage.setItem(USER_LS, name);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentName = input.value;
+  paintGreeting(currentName);
+  setName(currentName);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -14,7 +30,7 @@ function paintGreeting(text) {
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
-    // not
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
