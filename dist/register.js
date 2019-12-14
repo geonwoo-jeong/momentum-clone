1e5c6a2ef448bc4b0fc4edabd1bac5aa0619b4ea
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-12-14 22:45:48 +0900

    For Coding Challenge Upload

--- dist/register.js
+++
@@ -1,8 +0,0 @@
-var sw = (function () {
-    function init() {
-        if ("serviceWorker" in navigator) {
-            navigator.serviceWorker.register("../sw.js");
-        }
-    }
-    init();
-})();
